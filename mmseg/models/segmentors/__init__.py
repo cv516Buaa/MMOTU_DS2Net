2f7ef62e8be3b790da1592f3fd3f56aa7e90f04e
cv516Buaa <94512783+cv516Buaa@users.noreply.github.com>
2022-07-20 22:00:21 +0800

    Update __init__.py

--- mmseg/models/segmentors/__init__.py
+++ mmseg/models/segmentors/__init__.py
@@ -2,11 +2,8 @@
 from .base import BaseSegmentor
 from .cascade_encoder_decoder import CascadeEncoderDecoder
 from .encoder_decoder import EncoderDecoder
-from .encoder_decoder_forAdap import EncoderDecoder_forAdap
 
 ## added by LYU: 2022/04/22
 from .encoder_decoder_forDS2Net import EncoderDecoder_forDS2Net
-## added by LYU: 2022/05/17
-from .encoder_decoder_forEGAdap import EncoderDecoder_forEGAdap
 
-__all__ = ['BaseSegmentor', 'EncoderDecoder', 'CascadeEncoderDecoder', 'EncoderDecoder_forAdap', 'EncoderDecoder_forDS2Net', 'EncoderDecoder_forEGAdap']
+__all__ = ['BaseSegmentor', 'EncoderDecoder', 'CascadeEncoderDecoder', 'EncoderDecoder_forDS2Net']
